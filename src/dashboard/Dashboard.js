a1ca3ad6ffed6e1de5dfe9149b734e6a3cce942b
Norman Buruchara <normanburuchara90@gmail.com>
2021-04-08 14:25:47 -0500

    split dashboard into two sections

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -4,6 +4,52 @@ import styled from 'styled-components'
 
 const Styles = styled.div `
 
+body {
+    background-color: #154360;
+}
+
+    // - - PARENT DIV - - //
+
+.parent {
+    background-color: #154360;
+}
+
+    // - - PAGE DIMENSIONS - - //
+
+.dashCol:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+.dashEditor {
+    float: left;
+    width: 50%;
+    text-align: center;
+}
+
+.dashChat {
+    float: left;
+    width: 50%;
+    text-align: center;
+}
+
+
+// - - TEXT EDITOR - - //
+
+.dashEditor h3 {
+    color: white;
+    font-family: Karla;
+}
+
+// - - CHAT WINDOW - - //
+
+.dashChat h3 {
+    color: white;
+    font-family: Karla;
+}
+
+
 
 `
 
@@ -19,7 +65,16 @@ export default class Dashboard extends Component {
     render () {
         return(
             <Styles>
-                <h1>TEXT EDITOR AND EVERYTHIG ELSE </h1>
+                <div className="parent">
+                    <div className="dashCol">
+                        <div className="dashEditor">
+                            <h3>Text Editor goes here </h3>
+                        </div>
+                        <div className="dashChat">
+                            <h3>Chat window goes here</h3>
+                        </div>
+                    </div>
+                </div>
             </Styles>
         )
     }
